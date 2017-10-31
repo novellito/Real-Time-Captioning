679917ff84469da728578b43a7b4b45d0505a96a
HeyItsMario <mario.cordova.862@my.csun.edu>
2017-10-31 10:51:01 -0700

    Initial server is up and running. As well as a small CAS authentication flow.

---
+++ backend/config/config.js
@@ -0,0 +1,6 @@
+exports.casConfiguration = {
+  host: "auth.csun.edu",
+  port: 443,
+  path: "/cas/serviceValidate?service=http://localhost:8080/auth/cas&ticket=",
+  method: "GET"
+};
--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,36 @@
-const express    = require('express');
-const path       = require('path');
-const bodyParser = require('body-parser');
-const cors       = require('cors');
-const passport   = require('passport');
-const mongoose   = require('mongoose');
-const config     = require('./config/database');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const http = require("http");
+//const config = require("./config/database");
 
+const authRouter = require("./routes/auth");
 
-// Connect To Database
-mongoose.connect(config.database);
+// // Connect To Database
+// mongoose.connect(config.database);
 
-// On Connection
-mongoose.connection.on('connected', () => {
-  console.log('Connected to database '+config.database);
-});
+// // On Connection
+// mongoose.connection.on("connected", () => {
+//   console.log("Connected to database " + config.database);
+// });
 
-// On Error
-mongoose.connection.on('error', (err) => {
-  console.log('Database error: '+err);
-});
+// // On Error
+// mongoose.connection.on("error", err => {
+//   console.log("Database error: " + err);
+// });rs
 
 const app = express();
+const server = http.Server(app);
 
-// const users    = require('./routes/users');
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, "../dist")));
 
 // Port Number
 const port = process.env.PORT || 8080;
@@ -30,31 +38,22 @@ const port = process.env.PORT || 8080;
 // CORS Middleware
 app.use(cors());
 
-// Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Body Parser Middleware
-app.use(bodyParser.json());
-
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // require('./config/passport')(passport); passport here just in case we need it
 
 // app.use('/users', users);
+app.use("/auth", authRouter);
 
 // Index Route
-app.get('/', (req, res) => {
-  res.send('Invalid Endpoint');
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
-
-// app.get('*', (req, res)=>{
-//   res.sendFile(path.join(__dirname, 'public/index.html'));
-// });
-
+app.set("port", port);
 
 // Start Server
-app.listen(port, () => {
-  console.log('Server started on port '+port);
+server.listen(port, () => {
+  console.log("Server started on port: " + port);
 });
