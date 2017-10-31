dd6907296f1762add7adff1ca739872fdfa1c25b
Christian Trinidad <christian.trinidad.501@my.csun.edu>
2017-10-31 11:15:46 -0700

    added text editor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule,NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { QuillModule } from 'ngx-quill';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    QuillModule,
     MDBBootstrapModule.forRoot()
   ],
   providers: [],
---
+++ src/app/components/editor/editor.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { QuillEditorComponent } from 'ngx-quill/src/quill-editor.component';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
+// override p with div tag
+import Quill from 'quill';
+const Parchment = Quill.import('parchment');
+const Block = Parchment.query('block');
+
+Block.tagName = 'DIV';
+// or class NewBlock extends Block {}; NewBlock.tagName = 'DIV';
+Quill.register(Block /* or NewBlock */, true);
+// Add fonts to whitelist
+const Font = Quill.import('formats/font');
+// We do not add Aref Ruqaa since it is the default
+Font.whitelist = ['mirza', 'aref'];
+Quill.register(Font, true);
+
+@Component({
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.scss']
+})
+export class EditorComponent implements OnInit {
+
+
+  constructor() {
+   }
+
+  ngOnInit() {
+  }
+
+
+  logChange($event: any) {
+    console.log($event);
+  }
+
+}
