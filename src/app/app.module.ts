290cb4e3552ee19d54bbf4df28f4441f2fb49ecb
Mario <mario.cordova.862@my.csun.edu>
2017-10-31 22:03:27 -0700

    Created an initial student-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentListComponent
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   imports: [
