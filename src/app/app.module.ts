1646783d720343fe90916016de1276c80d6d4554
Rene Garcia-Ayon <rene.garciaayon.646@my.csun.edu>
2017-11-01 12:26:06 -0700

    Button component is now in correct directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
+import { ButtonComponent } from './components/button/button.component';
 // import { StatusComponent } from './components/status/status.component';
 import { SocketioComponent } from './components/socketio/socketio.component';
 
