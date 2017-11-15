7f7cf79cf3d39e9ee29028987f4f521394ed4a34
HeyItsMario <mario.cordova.862@my.csun.edu>
2017-11-15 11:57:55 -0800

    Made sure mongoose uses native promises. Refactored the way we store, retrieve, and delete admins. Using Promises

--- backend/controllers/admins.js
+++ backend/controllers/admins.js
@@ -5,14 +5,44 @@ const AdminModel = require("../models/admin");
 let AdminController = {};
 
 AdminController.getAllAdmins = (req, res) => {
-  console.log("Getting all admins");
+  let getAllAdminsPromise = AdminModel.find({}).exec();
+  getAllAdminsPromise
+    .then(admins => {
+      return res.status(200).json(admins);
+    })
+    .catch(err => {
+      return res.status(500).json({ error: err });
+    });
+};
 
-  AdminModel.find({}, (err, result) => {
-    if (err) {
+AdminController.storeAdmin = (req, res) => {
+  let admin = new AdminModel(req.body);
+  let createAdminPromise = admin.save();
+
+  createAdminPromise
+    .then(admin => {
+      return res.status(200).json(admin);
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).json({ error: err });
+    });
+};
+
+AdminController.destroyById = (req, res) => {
+  let adminID = req.params.id;
+  let findByIdAndRemovePromise = AdminModel.findByIdAndRemove(adminID).exec();
+
+  findByIdAndRemovePromise
+    .then(admin => {
+      return admin
+        ? res.status(200).json(admin)
+        : res.status(404).json({ error: `No Admin found with id: ${adminID}` });
+    })
+    .catch(err => {
+      console.log("Error: " + err.message);
       return res.status(500).json({ error: err.message });
-    }
-    res.json(result);
-  });
+    });
 };
 
 module.exports = AdminController;
--- backend/routes/admins.js
+++ backend/routes/admins.js
@@ -5,7 +5,12 @@ const controller = require("../controllers/admins");
 let router = express.Router();
 
 module.exports = function(io) {
-  // Get all admins
+  // Get all admins.
   router.get("/", controller.getAllAdmins);
+  // Create a new admin.
+  router.post("/", controller.storeAdmin);
+  // Deletes an admin with the specified ID.
+  router.delete("/id/:id", controller.destroyById);
+
   return router;
 };
