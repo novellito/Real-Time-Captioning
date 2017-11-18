e606b0caa131da037a9cfab0bd3411dc89595cc2
Christian Trinidad <christian.trinidad.501@my.csun.edu>
2017-11-17 16:32:52 -0800

    captioners can now type in separate rooms

--- backend/index.js
+++ backend/index.js
@@ -52,16 +52,20 @@ io.on("connection", socket => {
   socket.on('room', function(data){
     console.log(`user connected on room# ${data.room_id}`);
      socket.join(data.room_id);
+
+     socket.on("captionerDelta", del => {
+      console.log(del);
+     socket.to(data.room_id).emit("captions", del);
+    });
+
+
   }); 
 
   socket.on("disconnect", function() {
     console.log("user disconnected");
   });
 
-  socket.on("captionerDelta", del => {
-    console.log(del);
-    io.emit("captions", del);
-  });
+ 
 });
 
 // Start Server
