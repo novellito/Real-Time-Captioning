fd19a8b4a36c3a6466db1be39e240e2ca74313d1
Christian Trinidad <christian.trinidad.501@my.csun.edu>
2018-02-28 15:55:58 -0800

    added preliminary login functionality with jsonwebtokens

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,18 +1,35 @@
+const jwt = require('jsonwebtoken');
+
 let LoginController = {};
 
 LoginController.authenticate = (req,res) => {
+
+    const user = {
+        username: req.body.username
+    };
+
+    const token = jwt.sign(user, 'secret', {expiresIn:604800}) // 'secret will be env later on', expires in 1 week
+
 
     const usr = "test";
     const pw = "test";
     if(usr !== req.body.username || pw !== req.body.password) {
         res.send({msg:"invalid credentials"});
     } else {
-        res.send({msg:"success"});
+        // res.send({msg:"success"});ks
+        res.json({success:true, token: token,user:{username: user.username}});
+        
     }
     console.log(req.body.username);
     console.log(req.body.password);
     // res.end();
 
 }
+
+// LoginController.getProfile = (req,res) => {
+//     router.get('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+//         res.json({user: req.user});
+//       });
+// }
 
 module.exports = LoginController;
--- backend/routes/login.js
+++ backend/routes/login.js
@@ -2,11 +2,40 @@
 
 const express = require("express");
 const controller = require("../controllers/login");
+const jwt = require('jsonwebtoken');
+
+
+
 let router = express.Router();
 
 module.exports = function(io) {
 
   router.post("/", controller.authenticate);
+  router.get('/', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secret', (err, data) => {
+        if(err) {
+          res.sendStatus(403) // error            
+        } else {
+            console.log('login successful!');
+            res.json({data});
+        }
+    });
+  });
+
+  function verifyToken(req,res,next) {
+      // get header value
+      const bearerHeader = req.headers['authorization'];
+      if(typeof bearerHeader !== 'undefined') {
+        req.token = bearerHeader;
+        next();
+      } else {
+          res.sendStatus(403) // error
+      }
+
+  }
+//   router.get('/', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+//     res.json({user: req.user});
+//   });
  
   return router;
 };
