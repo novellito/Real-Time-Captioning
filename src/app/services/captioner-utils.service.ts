11a537421b3af159ac855b13cfd7b48e985a7737
evelyn araujo <evelin.araujo@metalab.csun.edu>
2018-05-01 22:04:31 -0700

    type

--- src/app/services/captioner-utils.service.ts
+++ src/app/services/captioner-utils.service.ts
@@ -20,7 +20,7 @@ export class CaptionerUtilsService {
     createTranscript(id) {
       const headers = new Headers();
       headers.append('Content-Type', 'application/json');
-      return this.http.post(`http://172.31.8.206::8080/api/transcripts`, {courseID: id} , {headers: headers})
+      return this.http.post(`http://172.31.8.206:8080/api/transcripts`, {courseID: id} , {headers: headers})
       .map(res => res.json());
     }
 
@@ -29,7 +29,7 @@ export class CaptionerUtilsService {
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
         return this.http // send the contents to the database
-          .put(`http://172.31.8.206::8080/api/transcripts/id/${id}`, {transcriptName: name}, { headers: headers })
+          .put(`http://172.31.8.206:8080/api/transcripts/id/${id}`, {transcriptName: name}, { headers: headers })
           .map(res => res.json());
     }
 
