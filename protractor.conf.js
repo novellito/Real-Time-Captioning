df05fd7a1bf9c67b540157d64ff9a374ff3a7497
evelyn araujo <evelin.araujo@metalab.csun.edu>
2018-05-01 22:34:30 -0700

    changing from http to https

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://172.31.8.206:4200/',
+  baseUrl: 'https://172.31.8.206:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
