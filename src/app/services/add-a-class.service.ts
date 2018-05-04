4b8ecd6b33904567505533749e42ecb4a5d1dade
evelyn araujo <evelin.araujo@metalab.csun.edu>
2018-05-04 02:10:27 -0700

    testing for open instance

--- src/app/services/add-a-class.service.ts
+++ src/app/services/add-a-class.service.ts
@@ -27,7 +27,7 @@ export class AddAClassService {
     };
     const headers = new Headers();
     headers.append('Content-Type', 'application/json; charset=UTF-8');
-    return this.http.post(`http://demo-load-balancer-1858473610.us-west-2.elb.amazonaws.com/api/classes`, data, {headers: headers})
+    return this.http.post(`http://34.209.157.38/api/classes`, data, {headers: headers})
     .map(res => res.json());
   }
 
@@ -38,13 +38,13 @@ export class AddAClassService {
     headers.append('Content-Type', 'application/json; charset=UTF-8');
     if (JSON.parse(localStorage.getItem('user')).role === 'captioner') {
 
-      return this.http.put(`http://demo-load-balancer-1858473610.us-west-2.elb.amazonaws.com/api/captionists/username/${JSON.parse(localStorage.getItem('user')).userID}`,
+      return this.http.put(`http://34.209.157.38/api/captionists/username/${JSON.parse(localStorage.getItem('user')).userID}`,
       data, {headers: headers})
       .map(res => res.json());
 
     } else {
 
-        return this.http.put(`http://demo-load-balancer-1858473610.us-west-2.elb.amazonaws.com/api/students/username/${JSON.parse(localStorage.getItem('user')).userID}`,
+        return this.http.put(`http://34.209.157.38/api/students/username/${JSON.parse(localStorage.getItem('user')).userID}`,
          data, {headers: headers})
         .map(res => res.json());
     }
